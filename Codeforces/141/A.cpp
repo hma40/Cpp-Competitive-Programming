710df92d8bf837f42928fb940d56a1cbc8d20703
Haokai Ma <mahaokai9@gmail.com>
2024-05-31 12:32:50 -0400

    .

---
+++ Codeforces/141/A.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define add push_back 
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define f first
+#define s second
+#define trav(a,x) for (auto& a: x)
+#define int long long
+#define vt vector
+ll mod = 1000000007;
+ll inf = 1e18;
+template<typename T1, typename T2>
+std::ostream& operator<<(std::ostream& os, const std::pair<T1, T2>& p) {
+    os << "(" << p.first << ", " << p.second << ")";
+    return os;
+}
+template<typename T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    os << "[ ";
+    for(const auto& elem : vec) {
+        os << elem << " ";
+    }
+    os << "]";
+    return os;
+}
+template<typename T> std::ostream& operator<<(std::ostream& os, const std::set<T>& s) {
+    os << "{ ";
+    for(const auto& elem : s) {
+        os << elem << " ";
+    }
+    os << "}";
+    return os;
+}
+template<typename T> std::ostream& operator<<(std::ostream& os, const std::multiset<T>& s) {
+    os << "{ ";
+    for(const auto& elem : s) {
+        os << elem << " ";
+    }
+    os << "}";
+    return os;
+}
+template<typename K, typename V> std::ostream& operator<<(std::ostream& os, const std::map<K, V>& m) {
+    os << "{ ";
+    for(const auto& pair : m) {
+        os << pair.first << " : " << pair.second << ", ";
+    }
+    os << "}";
+    return os;
+}
+signed main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    map<char, int> m;
+    // F0R(i, s.size()) m[s[i]]++;
+    string r,s,t;
+    cin >> r >> s >> t;
+    F0R(i, r.size()) m[r[i]]++;
+    F0R(i, s.size()) m[s[i]]++;
+    bool fail = false;
+    int x = t.size()-r.size()-s.size();
+    if(x!=0) {
+        fail=true;
+        // cout << "FAIL LINE 65" << endl;
+    }
+    F0R(i, t.size()) {
+        // cout << i << " " << t[i] << " " << m << endl;
+        if(m[t[i]]<=0) fail=true;
+        m[t[i]]--;
+    }
+    if(fail) cout << "NO" << endl;
+    else cout << "YES" << endl;
+    return 0;
+}
